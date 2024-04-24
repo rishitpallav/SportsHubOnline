01d0e4b1400485bf564aa3832c8b96e65531fb38
kirannemani-11 <robertlewandowski9reus11@gmail.com>
2024-04-24 15:44:05 -0500

    puss

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MainPage from "./Components/MainPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
